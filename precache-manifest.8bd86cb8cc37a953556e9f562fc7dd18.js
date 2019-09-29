998e7811674c1a98007f6822dc4c212c9d8680fb
GaneshSrambikal <ganesh.srambikal@gmail.com>
2019-09-29 20:53:35 +0530

    Updates

---
+++ precache-manifest.8bd86cb8cc37a953556e9f562fc7dd18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "151d5b0e0176295e91ffd37f1a57a85b",
+    "url": "/react00/index.html"
+  },
+  {
+    "revision": "c2175c91fab56e582b37",
+    "url": "/react00/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7b2bfcc1e728a45e4123",
+    "url": "/react00/static/js/2.980fa843.chunk.js"
+  },
+  {
+    "revision": "c2175c91fab56e582b37",
+    "url": "/react00/static/js/main.d4d3f468.chunk.js"
+  },
+  {
+    "revision": "60856c033cf934c59ded",
+    "url": "/react00/static/js/runtime-main.4a5eabf7.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react00/static/media/logo.25bf045c.svg"
+  }
+]);
